6aa985869d99b3e5d2119702e840904e24eb3560
sw-sprite <wei980622@gmail.com>
2022-01-26 12:06:03 -0600

    adding a couple solutions to 04

--- 04-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 04-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -6,10 +6,10 @@ class Solution:
         n = len(nums2)
         
         need_2 = True if ((m+n) % 2) == 0 else False
-        index_seek = (m+n) // 2
+        index_seek = ((m+n) // 2) + 1
 
-        temp_1 = 0
-        temp_2 = 0
+        temp_1 = -1
+        temp_2 = -1
 
         cur_nums1_index = 0
         cur_nums2_index = 0
@@ -19,29 +19,25 @@ class Solution:
         if n == 0: 
             return median(nums1)
         
-        if need_2:
-            for count in range(index_seek+1):
-                temp_2 = temp_1
-                if cur_nums1_index != n and cur_nums2_index != m:
-                    if nums1[cur_nums1_index] > nums2[cur_nums2_index]:
-                        temp_1 = nums2[cur_nums2_index]
-                        cur_nums2_index += 1
-                    else:
-                        m1 = nums1[cur_nums1_index]
-                        cur_nums1_index += 1
-                elif: cur_nums1_index < m:
+        for count in range(index_seek):
+            temp_2 = temp_1
+            if cur_nums1_index != m and cur_nums2_index != n:
+                if nums1[cur_nums1_index] > nums2[cur_nums2_index]:
+                    temp_1 = nums2[cur_nums2_index]
+                    cur_nums2_index += 1
+                else:
                     temp_1 = nums1[cur_nums1_index]
                     cur_nums1_index += 1
-                else:
-                    
-                    
-
-
-
-
-        return 0
-
-
+            elif cur_nums1_index < m:
+                temp_1 = nums1[cur_nums1_index]
+                cur_nums1_index += 1
+            else:
+                temp_1 = nums2[cur_nums2_index]
+                cur_nums2_index += 1
+        if need_2:
+            return (temp_1 + temp_2) / 2
+        else:
+            return temp_1
 
 tests = [
     (
